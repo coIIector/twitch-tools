8acc83579035cbf18dbf28c853745c7fcc140e60
coIIector <coiiector@users.noreply.github.com>
2020-02-03 00:55:06 +0100

    deploy

---
+++ docs/precache-manifest.51e916157a63a2ea222a069540cb4a68.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "643dbf21a1194a56574aa663cffbcde6",
+    "url": "/twitch-tools/index.html"
+  },
+  {
+    "revision": "adab6c5a65a784ae07e3",
+    "url": "/twitch-tools/static/css/2.8c34e9cc.chunk.css"
+  },
+  {
+    "revision": "1771e2a4ad5b5b0bc3b9",
+    "url": "/twitch-tools/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "adab6c5a65a784ae07e3",
+    "url": "/twitch-tools/static/js/2.2cf51021.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/twitch-tools/static/js/2.2cf51021.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1771e2a4ad5b5b0bc3b9",
+    "url": "/twitch-tools/static/js/main.bcc4e245.chunk.js"
+  },
+  {
+    "revision": "f379619f6414bfc8d62b",
+    "url": "/twitch-tools/static/js/runtime-main.154f3c37.js"
+  }
+]);
